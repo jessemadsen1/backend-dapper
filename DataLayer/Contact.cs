68e5d18ce17f726e228e8232dc9aaac0d9667b8b
Jesse Madsen <madsencad@gmail.com>
2021-10-10 19:18:54 -0600

    wow, I just ending up copy his project 3 folder

--- DataLayer/Contact.cs
+++ DataLayer/Contact.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 using System.Collections.Generic;
 
 namespace DataLayer
@@ -7,18 +8,15 @@ namespace DataLayer
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
-        
         public string LastName { get; set; }
-        
         public string Email { get; set; }
-        
         public string Company { get; set; }
-        
         public string Title { get; set; }
-        
+
+        [Computed]
         public bool IsNew => this.Id == default(int);
 
+        [Write(false)]
         public List<Address> Addresses { get; } = new List<Address>();
-
     }
 }
