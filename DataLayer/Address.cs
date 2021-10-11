bcc20c1642df19332ff6083a046532fa1fa9290d
Jesse Madsen <madsencad@gmail.com>
2021-10-10 18:51:11 -0600

    Installing Dapper

---
+++ DataLayer/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace DataLayer
+{
+    public class Address
+    {
+
+        public int Id { get; set; }
+        public int Contactld { get; set; }
+        public string AddressType { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public int Stateld { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
